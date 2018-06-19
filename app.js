fe4464335f52b3069fe479654b060cef05bf5032
zjhl2 <478791469@qq.com>
2018-06-19 22:25:22 +0800

    添加操作至1.4.2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 const koa = require('koa');
-const router = require('koa-router')();
+const router = require('./router/router.js');
 const session = require('koa-session');
 const  serve = require("koa-static");
 const bodyParser = require('koa-bodyparser');
@@ -22,73 +22,6 @@ app.use(midw.welcome);
 app.use(serve('./static'));
 
 app.use(router.routes());
-
-//get
-router.get("/getname", async (ctx) => {
-    ctx.body=ctx.session.id;
-})
-
-
-//post
-router.post('/signin', async (ctx) => {
-    var name=ctx.request.body.name;
-    var password=ctx.request.body.password;
-    console.log(name+ ' '+password);
-    if ((name==="zjhl2"&&password==="zjhl2")||(name==="zy"&&password=="zy"))
-    {
-        ctx.session.id=name;
-        ctx.body={code:1};
-    }
-    else 
-    {
-        ctx.body=
-        {
-            code:0,
-            err:"用户不存在"
-        };
-    }
-})
-
-router.post('/register',async (ctx) => {
-    var
-        name = ctx.request.body.name,
-        email = ctx.request.body.email,
-        tel = ctx.request.body.tel,
-        password = ctx.request.body.password ;
-    //var res = await DB.findDataByUser(name);
-    if (!name || !email ||!tel || ! password) {
-        ctx.body={
-            code:2,
-            err:"信息不全"
-        }
-    }
-    else {
-        ctx.body={
-            code:1
-        }
-    }
-})
-
-router.get('/signout',async (ctx) => {
-    ctx.session = null;
-})
-
-router.post('/submitorder', async (ctx, next) => {
-    var 
-        date=ctx.request.body.date||'',  //预约时间
-        address=ctx.request.body.address||'', //取货地址
-        type=ctx.request.body.type||'', //回收类型
-        remark=ctx.request.body.remark||'';  //备注
-    if (date && address && type) 
-        ctx.body={
-            code:1
-        };
-    else 
-        ctx.body={
-            code:0
-        }
-})
-
 
 app.listen(3000);
 console.log('app started at port 3000...');
---
+++ router/router.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const router = require('koa-router')();
+
+
+//1.1 登录
+router.post('/signin', async (ctx) => {
+    var name=ctx.request.body.name;
+    var password=ctx.request.body.password;
+    console.log(name+ ' '+password);
+    if ((name==="zjhl2"&&password==="zjhl2")||(name==="zy"&&password=="zy"))
+    {
+        ctx.session.id=name;
+        ctx.body={
+            code:1,
+            err:''
+        };
+    }
+    else 
+    {
+        ctx.body={
+            code:0,
+            err:"用户不存在"
+        };
+    }
+})
+
+
+//1.2 登出
+router.get('/signout',async (ctx) => {
+    ctx.session = null;
+})
+
+
+
+//1.3 注册
+router.post('/register',async (ctx) => {
+    var
+        name = ctx.request.body.name,
+        email = ctx.request.body.email,
+        tel = ctx.request.body.tel,
+        password = ctx.request.body.password ;
+    //var res = await DB.findDataByUser(name);
+    if (!name || !email ||!tel || ! password) {
+        ctx.body={
+            code:2,
+            err:"信息不全"
+        }
+    }
+    else {
+        ctx.body={
+            code:1,
+            err:''
+        }
+    }
+})
+
+//1.4.1
+//获取昵称
+router.get('/getname', async (ctx) => {
+    ctx.body=ctx.session.id;
+})
+
+//修改昵称
+router.post('/modifyname',async (ctx) => {
+    ctx.body={
+        "code":1,   //1 表示成功 ，2表示失败
+        "err":''   //错误信息
+    }
+})
+
+//1.4.2
+//获取手机号
+router.get('/gettel', async (ctx) => {
+    ctx.body="110";
+})
+
+//修改手机号
+router.post('/modifytel', async (ctx) => {
+    var newtel=ctx.request.body.newtel;
+    if (newtel)
+        ctx.body={
+            "code":1,   //1 表示成功 ，2表示失败
+            "err":''   //错误信息
+        }
+    else 
+        ctx.body={
+            "code":2,   //1 表示成功 ，2表示失败
+            "err":'新号码为空，修改失败'   //错误信息
+        }
+})
+
+//1.4.3
+//获取邮箱
+router.get('/getemail', async (ctx) => {
+    ctx.body="444@qq.com";
+})
+
+
+
+//2.1 提交出售订单
+router.post('/submitorder', async (ctx, next) => {
+    var 
+        date=ctx.request.body.date||'',  //预约时间
+        address=ctx.request.body.address||'', //取货地址
+        type=ctx.request.body.type||'', //回收类型
+        remark=ctx.request.body.remark||'';  //备注
+    if (date && address && type) 
+        ctx.body={
+            code:1
+        };
+    else 
+        ctx.body={
+            code:0
+        }
+})
+
+module.exports = router;
